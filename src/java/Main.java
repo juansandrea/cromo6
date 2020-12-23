9d5a3674d85efa63a3e8a8d828fdfced1d19f01f
juansandrea <65579324+juansandrea@users.noreply.github.com>
2020-12-23 14:14:47 -0300

    Add files via upload

--- src/java/Main.java
+++ src/java/Main.java
@@ -3,9 +3,15 @@ import throwable.CombustibleInsuficienteException;
 import throwable.PuertasInsuficientesException;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws CombustibleInsuficienteException, PuertasInsuficientesException {
+
+        ArrayList<Avion> estacionados = new ArrayList<>();
+
+
+
 
         ArrayList<Pasajero> pasajeros = new ArrayList<>();
 
@@ -26,14 +32,27 @@ public class Main {
 
 
 
-        Crucero crucero = new Crucero(20,8,50.00,tripulacion,pasajeros,"new york",80,100);
+        Crucero crucero = new Crucero(20,8,70.00,tripulacion,pasajeros,"new york",80,100);
         System.out.println(" Esta listo para despegar " + crucero.estaListoParaDespegar());
-        Avioneta avioneta = new Avioneta(5,2,10.00,tripulacion,pasajeros,"Rio de Janeio",20,30);
+        Avioneta avioneta = new Avioneta(5,2,50.00,tripulacion,pasajeros,"rio de janeiro",20,30);
         System.out.println(" Esta listo para despegar " + avioneta.estaListoParaDespegar());
 
-        avioneta.ingresarTripulante();
+        Crucero crucero1 = new Crucero(20,8,70.00,tripulacion,pasajeros,"new york",120,100);
+        Avioneta avioneta1 = new Avioneta(5,2,50.00,tripulacion,pasajeros,"rio de janeiro",20,30);
+        Crucero crucero2 = new Crucero(20,8,70.00,tripulacion,pasajeros,"new york",80,100);
+        Avioneta avioneta3 = new Avioneta(5,2,50.00,tripulacion,pasajeros,"rio de janeiro",20,30);
+        //avioneta.ingresarTripulante();
+        estacionados.add(crucero);
+        estacionados.add(crucero1);
+        estacionados.add(crucero2);
+        estacionados.add(avioneta);
+        estacionados.add(avioneta1);
+        System.out.println("crusero1 esta volando a "  + crucero1.getAltura() +" Pies y a una Velocidad de "+ crucero1.getVelocidad() + " Km/h" );
+        crucero1.aterrizarDeEmergencia();
+        System.out.println("crusero1 Solicito Aterrizar de Emergencia y Ahora esta volando a "  + crucero1.getAltura() +" Pies y a una Velocidad de "+ crucero1.getVelocidad() + " Km/h" );
 
-        avioneta.aterrizarDeEmergencia();
+        System.out.println("avioneta3 puede aterrizar. = " + avioneta3.puedeAterrizar(estacionados,100));
+
 
 
 
--- src/java/entidades/Avion.java
+++ src/java/entidades/Avion.java
@@ -3,11 +3,7 @@ package entidades;
 import throwable.CombustibleInsuficienteException;
 import throwable.PuertasInsuficientesException;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public abstract class Avion {
 
@@ -19,23 +15,24 @@ public abstract class Avion {
     private String destino;
     private int altura;
     private int velocidad;
-    private Map<String,Double> combustiblePorDestino;
+    private Map<String, Double> combustiblePorDestino;
 
 
-    public Avion(int largo, int puerta, double combustible,ArrayList<Tripulacion> tripulacion,ArrayList<Pasajero> pasajeros,String destino, int altura, int velocidad) {
+    public Avion(int largo, int puerta, double combustible, ArrayList<Tripulacion> tripulacion, ArrayList<Pasajero> pasajeros, String destino, int altura, int velocidad) {
         this.largo = largo;
         this.puerta = puerta;
         this.combustible = combustible;
         this.tripulacion = tripulacion;
         this.pasajeros = pasajeros;
+        this.destino = destino;
         this.altura = altura;
         this.velocidad = velocidad;
 
         combustiblePorDestino = new HashMap<>();
-        combustiblePorDestino.put("rio de janeiro",50.00);
-        combustiblePorDestino.put("new york",60.00);
-        combustiblePorDestino.put("barcelona",35.00);
-        combustiblePorDestino.put("madrid",40.00);
+        combustiblePorDestino.put("rio de janeiro", 50.00);
+        combustiblePorDestino.put("new york", 60.00);
+        combustiblePorDestino.put("barcelona", 35.00);
+        combustiblePorDestino.put("madrid", 40.00);
 
     }
 
@@ -81,39 +78,38 @@ public abstract class Avion {
     }
 
 
+    public boolean estaListoParaDespegar() throws CombustibleInsuficienteException, PuertasInsuficientesException {
 
-    public boolean estaListoParaDespegar()throws CombustibleInsuficienteException, PuertasInsuficientesException {
-
-        if (this.puerta < (this.tripulacion.size() + this.pasajeros.size()) /50 )
+        if (this.puerta < (this.tripulacion.size() + this.pasajeros.size()) / 50)
             throw new PuertasInsuficientesException();
 
         double combust = 0;
         Iterator it = combustiblePorDestino.keySet().iterator();
         while (it.hasNext()) {
-                String destinos = (String) it.next();
-                if (destinos.equals(this.destino)) {
-                    combust = combustiblePorDestino.get(destinos);
-                    break;
-                }
+            String destinos = (String) it.next();
+            if (destinos.equals(this.destino)) {
+                combust = combustiblePorDestino.get(destinos);
+                break;
             }
+        }
         if (combust <= this.combustible) {
-                return true;
+            return true;
         } else {
             throw new CombustibleInsuficienteException();
-            }
+        }
     }
 
 
-    public Tripulacion ingresarTripulante(){
+    public Tripulacion ingresarTripulante() {
         Tripulacion tripulante = new Tripulacion();
         Scanner teclado = new Scanner(System.in);
-        System.out.println(" Ingres el nombre del tripulante " );
+        System.out.println(" Ingres el nombre del tripulante ");
         tripulante.setNombre(teclado.nextLine());
-        System.out.println(" Ingres el apellido del tripulante " );
+        System.out.println(" Ingres el apellido del tripulante ");
         tripulante.setApellido(teclado.nextLine());
-        System.out.println(" Ingres el cargo del tripulante " );
+        System.out.println(" Ingres el cargo del tripulante ");
         tripulante.setCargo(teclado.nextLine());
-        System.out.println(" Ingres el dni del tripulante " );
+        System.out.println(" Ingres el dni del tripulante ");
         tripulante.setDNI(teclado.nextInt());
 
         tripulacion.add(tripulante);
@@ -122,8 +118,18 @@ public abstract class Avion {
     }
 
 
-
     public abstract void aterrizarDeEmergencia();
 
 
+    public boolean puedeAterrizar(List<Avion> estacionados, int largoTotal) {
+        int largoDeAviones = 0;
+        for (Avion avionlenght : estacionados){
+            largoDeAviones += avionlenght.largo;
+        }
+        return largoTotal - largoDeAviones >= 12 * this.largo;
+
+    }
 }
+
+
+
